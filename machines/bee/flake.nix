b57eb05e4058f7b8e3b69d45a5631f7ad0abf986
vegabook <thomas@scendance.fr>
2024-05-04 15:12:18 +0100

    flakes added first time

---
+++ machines/bee/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake to configure beelink mini pc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      }; 
+    };
+  in
+  {
+    nixosConfigurations = {
+      beeNixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+	modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
