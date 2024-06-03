0d4c9e6795be810c5b7ee5f4cfd059906ea0c223
vegabook <thomas.browne@delphos.co>
2024-06-03 17:36:20 +0100

    postgres localhost access

--- machines/delphox/configuration.nix
+++ machines/delphox/configuration.nix
@@ -177,9 +177,12 @@
   services.postgresql = {
     enable = true;
     ensureDatabases = [ "postgres" "tbrowne" ];
+    enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
-      local all       all     trust
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
     '';
   };
 
