ac0e37f8f194e0f911100a9831ac77120f410515
vegabook <thomas@scendance.fr>
2024-07-13 22:46:40 +0100

    with emoji for flakes

--- flakes/elixir_generic/flake.nix
+++ flakes/elixir_generic/flake.nix
@@ -1,63 +1,46 @@
 {
-  description = "A flake for building development environment of Phoenix project.";
+  description = "Python 3.11 development environment";
 
+  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
   };
 
   nixConfig = {
     bash-prompt-prefix = "🔮";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      with pkgs; {
-        devShells.default = mkShell {
-          buildInputs = [
-            erlangR25
-            beam.packages.erlangR25.elixir_1_14
-            nodejs
-          ]
-          ++ lib.optionals stdenv.isLinux [
-            # For ExUnit Notifier on Linux.
-            libnotify
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
 
-            # For file_system on Linux.
-            inotify-tools
-          ]
-          ++ lib.optionals stdenv.isDarwin ([
-            # For ExUnit Notifier on macOS.
-            terminal-notifier
-
-            # For file_system on macOS.
-            darwin.apple_sdk.frameworks.CoreFoundation
-            darwin.apple_sdk.frameworks.CoreServices
-          ]);
-
-          shellHook = ''
-            # allows mix to work on the local directory
-            mkdir -p .nix-mix
-            mkdir -p .nix-hex
-            export MIX_HOME=$PWD/.nix-mix
-            export HEX_HOME=$PWD/.nix-hex
-            export ERL_LIBS=$HEX_HOME/lib/erlang/lib
-
-            # concats PATH
-            export PATH=$MIX_HOME/bin:$PATH
-            export PATH=$MIX_HOME/escripts:$PATH
-            export PATH=$HEX_HOME/bin:$PATH
-
-            # enables history for IEx
-            export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
-
-            # Phoenix
-            mix archive.install hex phx_new
-          '';
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            beam.packages.erlang_27.elixir_1_17
+            rebar3
+            python312
+            python312Packages.grpcio
+            python312Packages.grpcio-tools
+            python312Packages.ipython
+            grpc-tools
+          ];
         };
-      }
-    );
+      });
+    };
 }
