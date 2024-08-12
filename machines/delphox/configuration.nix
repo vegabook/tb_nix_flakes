348bd0422c7bf11a71ec83af9887925e1621c02d
vegabook <thomas.browne@delphos.co>
2024-08-12 20:04:12 +0100

    with zoxide fixed

--- machines/delphox/configuration.nix
+++ machines/delphox/configuration.nix
@@ -132,6 +132,11 @@
     vim="nvim";
   };
 
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
