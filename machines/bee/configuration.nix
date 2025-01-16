70da138207dd4280729b93ba96c37fb7879d2a51
vegabook <thomas@scendance.fr>
2025-01-16 10:53:52 +0000

    bee samba

--- machines/bee/configuration.nix
+++ machines/bee/configuration.nix
@@ -103,6 +103,56 @@
 
   # List services that you want to enable:
 
+  services.samba = {
+  enable = true;
+  securityType = "user";
+  openFirewall = true;
+  settings = {
+    global = {
+      "workgroup" = "WORKGROUP";
+      "server string" = "smbnix";
+      "netbios name" = "smbnix";
+      "security" = "user";
+      #"use sendfile" = "yes";
+      #"max protocol" = "smb2";
+      # note: localhost is the ipv6 localhost ::1
+      "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+      "hosts deny" = "0.0.0.0/0";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
+    };
+    "public" = {
+      "path" = "/mnt/sea5/";
+      "browseable" = "yes";
+      "read only" = "no";
+      "guest ok" = "yes";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "force user" = "username";
+      "force group" = "groupname";
+    };
+    "private" = {
+      "path" = "/mnt/sea5/";
+      "browseable" = "yes";
+      "read only" = "no";
+      "guest ok" = "no";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "force user" = "username";
+      "force group" = "groupname";
+    };
+  };
+};
+# for samba advertise shares to windows
+services.samba-wsdd = {
+  enable = true;
+  openFirewall = true;
+};
+# for samba
+networking.firewall.enable = true;
+networking.firewall.allowPing = true;
+
+
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
