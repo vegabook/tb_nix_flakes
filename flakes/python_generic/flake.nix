7a7b1d99e05376c02d1599217fa4a5df066e8cfb
tbrowne <thomas.browne@mac.com>
2025-10-21 22:11:22 +0100

    python without constant venvs

--- flakes/python_generic/flake.nix
+++ flakes/python_generic/flake.nix
@@ -28,23 +28,48 @@
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
           packages = with pkgs; [
-            python313
-            python313Packages.pandas
-            python313Packages.numpy
-            python313Packages.scipy
-            python313Packages.scikit-learn
-            python313Packages.matplotlib
-            python313Packages.ipython
-            python313Packages.requests
-            python313Packages.aiohttp
-            python313Packages.gql
-            python313Packages.pathlib2
-            python313Packages.pip
-            python313Packages.pyarrow
-            python313Packages.polars
-
+            python312
+            python312Packages.grpcio
+            python312Packages.grpcio-tools
+            python312Packages.ipython
+            python312Packages.certifi
+            python312Packages.cryptography
+            python312Packages.rich
+            python312Packages.numpy
+            python312Packages.pandas
+            python312Packages.polars
+            python312Packages.psutil
+            python312Packages.pytest
+            python312Packages.poetry-core
+            python312Packages.loguru
+            python312Packages.requests
+            python312Packages.vulture
+            ruff
+            poetry
+            grpc-tools
+            openssl_3_3
+            certstrap
           ];
+
+          shellHook = ''
+            if [ -z "$NIX_SHELL_NESTED" ]; then
+              export NIX_SHELL_NESTED=1
+              alias ipy="ipython --nosep"
+              alias exit="deactivate && exit"
+              export PS1="🧢 \e[38;5;211m\]g\e[38;5;111mBLP\[\e[0m $PS1"
+              if [ ! -d venv ]; then
+                echo "Creating virtual environment in ./venv"
+                python -m venv venv
+              fi
+              echo "Activating virtual environment"
+              source venv/bin/activate
+            else
+              echo "Nested nix-shell detected, skipping venv creation and activation"
+            fi
+          '';
         };
       });
     };
 }
+
+
