fadde6d5996dcc0cefb5a7a9f2dafedac1e77914
vegabook <thomas.browne@mac.com>
2026-03-17 13:10:32 +0000

    no home manager

--- machines/mini10g/switch.sh
+++ machines/mini10g/switch.sh
@@ -1,2 +1,2 @@
-darwin-rebuild switch --flake /Users/tbrowne/.config/tb_nix_flakes/machines/mini10g#mini10g
+sudo darwin-rebuild switch --flake /Users/tbrowne/.config/tb_nix_flakes/machines/mini10g#mini10g
 
