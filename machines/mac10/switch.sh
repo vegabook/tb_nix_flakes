3caa99d28f6134dba681d281acba0f672744bb9f
vegabook <thomas.browne@mac.com>
2026-03-21 12:03:27 +0000

    mac10 replaces mini10g

---
+++ machines/mac10/switch.sh
@@ -0,0 +1,2 @@
+sudo darwin-rebuild switch --flake /Users/tbrowne/.config/tb_nix_flakes/machines/mac10#mac10
+
--- machines/mini10g/switch.sh
+++
@@ -1,2 +0,0 @@
-sudo darwin-rebuild switch --flake /Users/tbrowne/.config/tb_nix_flakes/machines/mini10g#mini10g
-
